3508e346dcee555ca9b843f61ec3d5463ba34656
Huang0520 <86014561+huang0520@users.noreply.github.com>
2024-11-21 23:51:33 +0800

    Update .default_install_pkg/uv.sh Update .zshrc

--- .default_install_pkg/uv.sh
+++ .default_install_pkg/uv.sh
@@ -2,6 +2,8 @@
 
 if command -v uv >/dev/null 2>&1; then
   uv tool install ruff
+  uv tool install "python-lsp-server" --with "pylsp-rope"
+  uv tool install basedpyright
 
 else
   echo "Not detect uv!"
